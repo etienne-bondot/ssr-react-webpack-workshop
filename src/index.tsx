86fcec943cab23738e4df3ba928a4fb0359facf2
Etienne Bondot <etienne.bondot@mwm.io>
2023-08-04 14:27:57 +0200

    feat: split routes from index

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,12 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import routes from "./routes";
 
 const container = document.getElementById("root");
 
 const root = createRoot(container!);
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 root.render(<RouterProvider router={router} />);
