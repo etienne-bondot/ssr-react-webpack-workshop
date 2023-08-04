aeb9c497ebf08f954fb7d38465a05ebac3ea9bc0
Etienne Bondot <etienne.bondot@mwm.io>
2023-08-04 14:16:47 +0200

    feat: add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import { createRoot } from "react-dom/client";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App";
 
 const container = document.getElementById("root");
+
 const root = createRoot(container!);
-root.render(<App />);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+root.render(<RouterProvider router={router} />);
